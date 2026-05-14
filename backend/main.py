932700d6496fb21b4dc65b1ee395d33aead80dc9
pfeu00 <patrick.feuz@edu.teko.ch>
2026-05-14 20:08:58 +0200

    update

--- backend/main.py
+++ backend/main.py
@@ -141,3 +141,7 @@ def get_dashboard():
         "ausgeliehen": ausgeliehen,
         "defekt": defekt
     }
+
+    if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
