6bec81786872003de0652dcc3dfc0a95623c6e82
pfeu00 <patrick.feuz@edu.teko.ch>
2026-05-16 15:53:13 +0200

    update

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://inventarfrontend-hsfubmgge0arhag8.germanywestcentral-01.azurewebsites.net"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
